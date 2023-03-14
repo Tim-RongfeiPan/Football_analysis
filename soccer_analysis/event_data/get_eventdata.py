f0d289c2aa665f9d7353605e6e04ce0d0d2ccd63
Rongfei Pan <rongfei@kth.se>
2023-03-14 16:23:48 +0100

    get_eventdata value

--- soccer_analysis/event_data/get_eventdata.py
+++ soccer_analysis/event_data/get_eventdata.py
@@ -11,6 +11,18 @@
 
 # here put the import lib
 
+from http.client import INSUFFICIENT_STORAGE
+from json import load
 import openpyxl
 import sys
 import re
+from openpyxl import load_workbook
+
+if __name__ == '__main__':
+    path = 'datasets/IK Frej Täby-IF Brommapojkarna(0-1).xlsx'
+
+    wb = load_workbook(path)
+    data = wb['Sheet1']
+    # value = data['A2']
+    value = data.cell(1, 2)
+    print(value.value)
