415f2603ad24f594784aa786af14d9af3f97f754
Rongfei Pan <rongfei@kth.se>
2023-03-22 16:26:22 +0100

    get video data by time updated

--- soccer_analysis/event_data/get_eventdata.py
+++ soccer_analysis/event_data/get_eventdata.py
@@ -131,31 +131,50 @@ class Get_Eventdata:
 class Get_Videodata:
     """Get_Videodata."""
 
-    def __init__(self, video_path, time_offset):
+    def __init__(self, video_path, time_offset=0):
+        """initialization
+
+        Args:
+            video_path (_string_): path of video
+            time_offset (_float_): secs, time of beginning of game in the video
+        """
         super(Get_Videodata, self).__init__()
-        self.video = cv2.VideoCapture(video_path)
+        self.video_path = video_path
+        self.video = cv2.VideoCapture(self.video_path)
         self.fps = self.video.get(cv2.CAP_PROP_FPS)
         self.time_offset = time_offset * self.fps  #number of f rames
+        self.num_frame = self.video.get(7)
 
-    def get_videodata_bytime(self, steps_frame, *time):
-        if len(time) == 1:
-            time = (time[0] * self.fps - steps_frame,
-                    time[0] * self.fps + steps_frame)
-        t1 = int(time[0]) + self.time_offset * self.fps
-        t2 = int(time[1]) + self.time_offset * self.fps
-        i = 0
-        out = []
-        while self.video.isOpened():
-            j, frame = self.video.read()
-            if j:
-                i += 1
-                if i >= t1 and i <= t2:
-                    out.append(frame)
-                elif i > t2:
-                    return out
-            else:
-                print('end')
-        return out
+    def get_videodata_bytime(self, steps_frame, time: datetime.time):
+        """ get video data by time
+
+        Args:
+            steps_frame (int): number of frame around event
+            time (datetime.time): time
+
+        Returns:
+            frame
+        """
+        #time in second
+        time = time.hour * 60 * 60 + time.minute * 60 + time.second
+        time = (time * self.fps - steps_frame, time * self.fps + steps_frame)
+        t1 = int(time[0] + self.time_offset)
+        # t2 = int(time[1] + self.time_offset)
+        self.video.set(cv2.CAP_PROP_POS_FRAMES, int(t1))
+        j, frame = self.video.read()
+        # cv2.imshow('sd', frame)
+        # cv2.waitKey()
+        # while self.video.isOpened():
+        #     j, frame = self.video.read()
+        #     if j:
+        #         i += 1
+        #         if i >= t1 and i <= t2:
+        #             out.append(frame)
+        #         elif i > t2:
+        #             return out
+        #     else:
+        #         print('errors occured')
+        return frame
 
 
 if __name__ == '__main__':
@@ -171,6 +190,7 @@ if __name__ == '__main__':
     # print('=============')
     # print(out[0])
     path = 'datasets/ettan_test.mp4'
+    dtime = datetime.time(0, 0, 46)
     vi = Get_Videodata(path, 0)
-    out = vi.get_videodata_bytime(2, 10)
+    out = vi.get_videodata_bytime(2, dtime)
     print(len(out))
--- soccer_analysis/test.py
+++ soccer_analysis/test.py
@@ -10,6 +10,19 @@
 '''
 
 # here put the import lib
+import datetime
+from event_data.get_eventdata import Get_Eventdata, Get_Videodata
+from demo_test import run
 
 if __name__ == "__main__":
-    pass
+    video_path = 'datasets/test//ifsylvia-tabyfk.mp4'
+    event_path = 'datasets/test/IF Sylvia - Täby FK (1-2).xlsx'
+    info_path = 'datasets/test/IF Sylvia - Täby FK.txt'
+
+    time_shot = datetime.time(0, 45, 55)
+
+    xlsx = Get_Eventdata(event_path)
+    video = Get_Videodata(video_path, 0)
+
+    out = video.get_videodata_bytime(5, time_shot)
+    print(len(out))
