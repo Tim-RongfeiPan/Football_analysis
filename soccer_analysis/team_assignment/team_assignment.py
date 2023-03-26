eee0b29ccf6778991cbbfe459a1fa3cef9518483
Rongfei Pan <rongfei@kth.se>
2023-03-26 20:30:51 +0200

    demo updated, class

--- soccer_analysis/team_assignment/team_assignment.py
+++ soccer_analysis/team_assignment/team_assignment.py
@@ -8,34 +8,42 @@ from loguru import logger
 from sklearn.cluster import KMeans
 
 # RGB code
-All_colors = {'Yellow': (195, 195, 10),
-              'White': (235, 235, 235),
-              'Green': (10, 235, 10),
-              'Blue': (10, 10, 205),
-              'Black': (0, 0, 0),
-              'Red': (235, 30, 30),
-              'Purple': (160, 32, 240),
-              'Orange': (255, 165, 30),
-              'Dark': (85, 85, 85)}
+All_colors = {
+    'Yellow': (195, 195, 10),
+    'White': (235, 235, 235),
+    'Green': (10, 235, 10),
+    'Blue': (10, 10, 235),
+    'Black': (10, 10, 10),
+    'Red': (245, 30, 30),
+    'Purple': (160, 32, 240),
+    'Orange': (235, 165, 30),
+    'Dark': (85, 85, 85)
+}
 
-file_dict = {'frej-bp.txt': ['White-Yellow.png', 'Yellow-White.png'],
-             'IF Brommapojkarna - Örebro Syrianska IF.txt': ['Yellow-RedBlack.png', 'RedBlack-Yellow.png'],
-             'IF Sylvia - Team TG FF.txt': ['BlackWhite-Red.png', 'Red-BlackWhite.png'],
-             'IFK Berga - IF Sylvia.txt': ['Orange-White.png', 'White-Orange.png'],
-             'IFK Värnamo - FC Linköping City.txt': ['White-Blue.png', 'Blue-White.png'],
-             'Nyköpings BIS - IFK Haninge.txt': ['Black-Red.png', 'Red-Black.png'],
-             'Qviding FIF - Landskrona BoIS.txt': ['Dark-BlackWhite.png', 'BlackWhite-Dark.png'],
-             'Qviding FIF - Lunds BK.txt': ['Red-Yellow.png', 'Yellow-Red.png'],
-             'Qviding FIF - Skövde AIK.txt': ['Red-Green.png', 'Green-Red.png'],
-             'Skövde AIK - FC Linköping City.txt': ['Blue-Red.png', 'Red-Blue.png'],
-             'Torns IF - Motala AIF FK.txt': ['Orange-White2.png', 'White-Orange2.png'],
-             'Tvååkers IF - Landskrona BoIS.txt': ['BlackWhite-Red2.png', 'Red-BlackWhite2.png'],
-             'Utsikten - Lindome GIF.txt': ['Dark-White.png', 'White-Dark.png']}
+file_dict = {
+    'frej-bp.txt': ['White-Yellow.png', 'Yellow-White.png'],
+    'IF Brommapojkarna - Örebro Syrianska IF.txt':
+    ['Yellow-RedBlack.png', 'RedBlack-Yellow.png'],
+    'IF Sylvia - Team TG FF.txt': ['BlackWhite-Red.png', 'Red-BlackWhite.png'],
+    'IFK Berga - IF Sylvia.txt': ['Orange-White.png', 'White-Orange.png'],
+    'IFK Värnamo - FC Linköping City.txt':
+    ['White-Blue.png', 'Blue-White.png'],
+    'Nyköpings BIS - IFK Haninge.txt': ['Black-Red.png', 'Red-Black.png'],
+    'Qviding FIF - Landskrona BoIS.txt':
+    ['Dark-BlackWhite.png', 'BlackWhite-Dark.png'],
+    'Qviding FIF - Lunds BK.txt': ['Red-Yellow.png', 'Yellow-Red.png'],
+    'Qviding FIF - Skövde AIK.txt': ['Red-Green.png', 'Green-Red.png'],
+    'Skövde AIK - FC Linköping City.txt': ['Blue-Red.png', 'Red-Blue.png'],
+    'Torns IF - Motala AIF FK.txt': ['Orange-White2.png', 'White-Orange2.png'],
+    'Tvååkers IF - Landskrona BoIS.txt':
+    ['BlackWhite-Red2.png', 'Red-BlackWhite2.png'],
+    'Utsikten - Lindome GIF.txt': ['Dark-White.png', 'White-Dark.png']
+}
 
 
 def detect_color(img, colors, debug=False):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img.reshape((img.shape[1]*img.shape[0], 3))
+    img = img.reshape((img.shape[1] * img.shape[0], 3))
 
     kmeans = KMeans(n_clusters=4, n_init=10)
     s = kmeans.fit(img)
@@ -47,7 +55,7 @@ def detect_color(img, colors, debug=False):
 
     for i in range(len(centroid)):
         j = labels.count(i)
-        j = j/(len(labels))
+        j = j / (len(labels))
         percent.append(j)
 
     detected_color = centroid[np.argsort(percent)[-2]]
@@ -61,8 +69,8 @@ def detect_color(img, colors, debug=False):
         logger.info(detected_color)
 
     assigned_color = closest_color(list_of_colors, detected_color)[0]
-    assigned_color = (int(assigned_color[0]), int(
-        assigned_color[1]), int(assigned_color[2]))
+    assigned_color = (int(assigned_color[0]), int(assigned_color[1]),
+                      int(assigned_color[2]))
 
     # colorName = colors[0] if All_colors[colors[0]
     #                                         ] == assigned_color else colors[1]
@@ -78,7 +86,7 @@ def detect_color(img, colors, debug=False):
 def closest_color(list_of_colors, color):
     colors = np.array(list_of_colors)
     color = np.array(color)
-    distances = np.sqrt(np.sum((colors-color)**2, axis=1))
+    distances = np.sqrt(np.sum((colors - color)**2, axis=1))
     index_of_shortest = np.where(distances == np.amin(distances))
     shortest_distance = colors[index_of_shortest]
     return shortest_distance
@@ -88,8 +96,7 @@ def team_assignment(crop, infoFile, debug=False):
     # detect the crop image to find the corresponding team from infoFile
     with open(infoFile, 'r') as f:
         data = f.read()
-        two_colors = re.findall(
-            r'team, (.*?) [J,j]erseys[)]', data)
+        two_colors = re.findall(r'team, (.*?) [J,j]erseys[)]', data)
         colors = []
         for color in two_colors:
             if color.find('/') != -1:
@@ -101,9 +108,11 @@ def team_assignment(crop, infoFile, debug=False):
                     if color[0].islower():
                         newcolors[index] = color.capitalize()
                 newcolor = ((All_colors[newcolors[0]][0] +
-                             All_colors[newcolors[1]][0])/2, (All_colors[newcolors[0]][1] +
-                                                              All_colors[newcolors[1]][1])/2, (All_colors[newcolors[0]][2] +
-                                                                                               All_colors[newcolors[1]][2])/2)
+                             All_colors[newcolors[1]][0]) / 2,
+                            (All_colors[newcolors[0]][1] +
+                             All_colors[newcolors[1]][1]) / 2,
+                            (All_colors[newcolors[0]][2] +
+                             All_colors[newcolors[1]][2]) / 2)
                 All_colors[newcolorName] = newcolor
                 colors.append(newcolorName)
             else:
@@ -111,8 +120,7 @@ def team_assignment(crop, infoFile, debug=False):
         for index, color in enumerate(colors):
             if color[0].islower():
                 colors[index] = color.capitalize()
-    color, colorName = detect_color(
-        crop, colors, debug=debug)
+    color, colorName = detect_color(crop, colors, debug=debug)
     return colorName
 
 
@@ -132,12 +140,11 @@ if __name__ == '__main__':
     correct = 0
 
     for (txt_file, image_file) in file_dict.items():
-        txt_path = root_path+'/'+txt_file
+        txt_path = root_path + '/' + txt_file
 
         with open(txt_path, 'r') as f:
             data = f.read()
-            two_colors = re.findall(
-                r'team, (.*?) [J,j]erseys[)]', data)
+            two_colors = re.findall(r'team, (.*?) [J,j]erseys[)]', data)
             colors = []
             for color in two_colors:
                 if color.find('/'):
@@ -148,21 +155,20 @@ if __name__ == '__main__':
                 if color[0].islower():
                     colors[index] = color.capitalize()
         for image_path in image_file:
-            crop_path = root_path+'/'+image_path
+            crop_path = root_path + '/' + image_path
             results = image_path.split('-')[0]
-            crop_path = root_path+'/'+image_path
+            crop_path = root_path + '/' + image_path
             crop = cv2.imread(crop_path)
             colorName = team_assignment(crop, txt_path)
             if colorName in results:
                 correct += 1
             else:
-                logger.info(
-                    f'processing file: {txt_file} and {image_path},' +
-                    f'colors: {colors}, results: {results}')
+                logger.info(f'processing file: {txt_file} and {image_path},' +
+                            f'colors: {colors}, results: {results}')
                 logger.info(f'prac results: {colorName}')
                 colorName = team_assignment(crop, txt_path, debug=True)
             all += 1
 
-    acc = correct/all
+    acc = correct / all
     logger.info(f'{all},{correct}')
     logger.info(f'Accuracy: {acc}')
--- soccer_analysis/test.py
+++ soccer_analysis/test.py
@@ -14,6 +14,7 @@ import datetime
 from event_data.get_eventdata import Get_Eventdata, Get_Videodata
 from demo_test import run
 import cv2
+import os
 from pathlib import Path
 
 if __name__ == "__main__":
@@ -22,18 +23,39 @@ if __name__ == "__main__":
     info_path = 'datasets/test/IF Sylvia - Täby FK.txt'
 
     time_shot = datetime.time(0, 45, 55)
-
+    test_type = 'Shots'
+    time_offset = 0.65
     xlsx = Get_Eventdata(event_path)
-    video = Get_Videodata(video_path, 0)
+    video = Get_Videodata(video_path, time_offset)
 
-    out = video.get_videodata_bytime(5, time_shot)
-    cv2.imwrite('runs/temp/testing_temp.jpg', out)
-    source = 'runs/temp/testing_temp.jpg'
-    weight_path = Path('datasets/best.pt')
-    run(source=source,
-        yolo_weights=weight_path,
-        show_team=info_path,
-        no_strongSort=True,
-        show_perstrans=True,
-        save_vid=True,
-        save_crop=True)
+    test_list = xlsx.get_eventdata_byevent(test_type)
+    print(len(test_list))
+    print(len(test_list[0]))
+    print(test_list[0])
+
+    # save screen shot
+    for index, event in enumerate(test_list):
+        time_shot = event[1]
+        out = video.get_videodata_bytime(5, time_shot)
+        jpg_name = 'runs/temp/testing_temp_' + str(index) + '.jpg'
+        cv2.imwrite(jpg_name, out)
+        source = jpg_name
+        weight_path = Path('datasets/best.pt')
+        im0, seg_map, model_image, retrieved_image = run(
+            source=source,
+            yolo_weights=weight_path,
+            show_team=info_path,
+            no_strongSort=True,
+            show_perstrans=True,
+            save_vid=True,
+            save_crop=True)
+
+        if not os.path.exists('runs/save/' + str(index)):
+            os.makedirs('runs/save/' + str(index))
+
+        cv2.imwrite('runs/save/' + str(index) + '/test_im0.jpg', im0)
+        cv2.imwrite('runs/save/' + str(index) + '/test_retrieved_image.jpg',
+                    retrieved_image)
+        cv2.imwrite('runs/save/' + str(index) + '/test_seg_map.jpg', seg_map)
+        cv2.imwrite('runs/save/' + str(index) + '/test_model_image.jpg',
+                    model_image)
